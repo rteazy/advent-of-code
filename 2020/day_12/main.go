a8e9accd08f5883a0f1e47431a1fae8cac442928
Ray Tran <raytran.dev@gmail.com>
2020-12-11 23:38:18 -0800

    day 12

---
+++ 2020/day_12/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+var inputFile = flag.String("inputFile", "input.txt", "The relative filepath to the problem input")
+
+func partOne() {
+	actions := parseInput()
+	i, j := 0, 0
+	directions := []rune{'N', 'E', 'S', 'W'}
+	dirIndex := 1
+	for _, act := range actions {
+		switch act.direction {
+		case 'N':
+			i -= act.value
+		case 'S':
+			i += act.value
+		case 'W':
+			j -= act.value
+		case 'E':
+			j += act.value
+		case 'L':
+			offset := (act.value / 90) % len(directions)
+			dirIndex -= offset
+			if dirIndex < 0 {
+				dirIndex = len(directions) + dirIndex
+			}
+		case 'R':
+			dirIndex = (dirIndex + (act.value / 90)) % len(directions)
+		case 'F':
+			currDirection := directions[dirIndex]
+			switch currDirection {
+			case 'E':
+				j += act.value
+			case 'W':
+				j -= act.value
+			case 'N':
+				i -= act.value
+			case 'S':
+				i += act.value
+			}
+		}
+	}
+	fmt.Println(i + j)
+}
+
+func partTwo() {
+	actions := parseInput()
+	waypointX, waypointY := 10.0, 1.0
+	positionX, positionY := 0.0, 0.0
+
+	for _, act := range actions {
+		val := float64(act.value)
+		switch act.direction {
+		case 'N':
+			waypointY += val
+		case 'S':
+			waypointY -= val
+		case 'W':
+			waypointX -= val
+		case 'E':
+			waypointX += val
+		case 'L', 'R':
+			rotationRadians := val * (math.Pi / 180.0)
+			if act.direction == 'R' {
+				rotationRadians *= -1
+			}
+			newWayPointX := waypointX*math.Cos(rotationRadians) - waypointY*math.Sin(rotationRadians)
+			newWayPointY := waypointY*math.Cos(rotationRadians) + waypointX*math.Sin(rotationRadians)
+			waypointX, waypointY = newWayPointX, newWayPointY
+		case 'F':
+			positionX += waypointX * val
+			positionY += waypointY * val
+		}
+	}
+
+	fmt.Println(math.Abs(positionX) + math.Abs(positionY))
+}
+
+type action struct {
+	direction rune
+	value     int
+}
+
+func parseInput() []action {
+	flag.Parse()
+	f, err := os.Open(*inputFile)
+	if err != nil {
+		log.Fatal("Failed to open the problem input file")
+	}
+
+	actions := []action{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		dir, val := line[0], line[1:]
+		value, err := strconv.Atoi(val)
+		if err != nil {
+			log.Fatal("Failed to convert the input")
+		}
+		actions = append(actions, action{rune(dir), value})
+	}
+	return actions
+}
+
+func main() {
+	partOne()
+	partTwo()
+}
